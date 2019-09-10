af1a06458044ebff07e6dca7a59dda7f16190957
Martin Hrbac <martin.hrbac@siemens.com>
2019-09-10 03:02:45 -0400

    added pce view

--- Code/CreateView.sql
+++ Code/CreateView.sql
@@ -17,4 +17,19 @@ ALTER TABLE public.vstatepopulation
     OWNER TO postgres;
 
 
+-- DROP VIEW public.vstatepopulation;
+
+CREATE OR REPLACE VIEW public.vstatepce AS
+ SELECT s.name AS state,
+    s.name_a2 AS state_a2,
+    p.pce_total_state,
+    p.pce_total_percapita,
+    p.pce_food_beverages,
+    p.year
+   FROM state s
+     JOIN state_pce p ON s.id = p.state_id;
+
+ALTER TABLE public.vstatepopulation
+    OWNER TO postgres;
+	
 
